389e17a0d7eb945ff203e31aaff07916af67c31f
Termoplan <alex223666@gmail.com>
2020-09-03 22:59:42 +0300

    developing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GameBoard from './pages/GameBoard'
+import Landing from './pages/Landing'
+import Route from 'react-router-dom/Route'
+import Switch from 'react-router-dom/Switch'
+import './static/styles/index.sass'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path='/'>
+          <Landing />
+        </Route>
+        <Route exact path='/game'>
+          <GameBoard />
+        </Route>
+      </Switch>
     </div>
   );
 }
